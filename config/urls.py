9cca63f847866fd096c65208c6f83f5a16feab7b
tiagolch <tiagolch@hotmail.com>
2022-10-02 23:35:54 +0100

    Configured gunicorn and crispy
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
+from core.views import new_subscription
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', new_subscription, name='new_subscription'),
 ]
