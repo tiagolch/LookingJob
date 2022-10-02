c5f5cf5542acf4f9c79acadd6d89880c1f514d23
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2022-10-03 00:05:20 +0100

    folder db

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + str(BASE_DIR)+'db.sqlite3'
+default_dburl = 'sqlite://' + str(BASE_DIR)+'db.sqlite3'
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
