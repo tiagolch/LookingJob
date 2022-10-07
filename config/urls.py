c4c749ab422e905e47cc3963d065c7d58b345b80
tiagolch <tiagolch@hotmail.com>
2022-10-07 23:34:52 +0100

    Add login
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import new_subscription
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', new_subscription, name='new_subscription'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Companies, Documents
 from .forms import CompaniesForms
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def new_subscription(request):
     form = CompaniesForms(request.POST or None)
     if form.is_valid():
