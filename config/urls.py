744d2cb284c7a92ff41201836663a9118a57a16b
tiagolch <tiagolch@hotmail.com>
2022-10-12 20:55:17 +0100

    Configured CRUD
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from core.views import new_subscription
+from core.views import edit_subscription, new_subscription, list_subscriptions, delete_subscription
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    path('', new_subscription, name='new_subscription'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('', new_subscription, name='new_subscription'),
+    path('list_subscriptions/', list_subscriptions, name='list_subscriptions'),
+    path('edit_subscription/<int:pk>/', edit_subscription, name='edit_subscription'),
+    path('delete_subscription/<int:pk>/', delete_subscription, name='delete_subscription'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from core.models import Companies
 from .forms import CompaniesForms
 from django.contrib.auth.decorators import login_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.db import transaction
+from django.urls import reverse_lazy
+
+
+@login_required
+def list_subscriptions(request):
+    user = request.user
+    data = Companies.objects.filter(user=user)
+    context = {"data": data }
+    return render(request, "core/list_subscriptions.html", context)
 
 
 @login_required
@@ -14,13 +27,20 @@ def new_subscription(request):
     return render(request, "core/new_subscription.html", {'form': form})
 
 
+@login_required
+def edit_subscription(request, pk):
+    data = Companies.objects.get(pk=pk)
+    form = CompaniesForms(instance = data)
+    if request.method == "POST": 
+        form = CompaniesForms(request.user, request.POST, instance = data)
+        if form.is_valid():
+            form.save()
+        return redirect('/list_subscriptions/')
+    return render(request, "core/edit_subscription.html", {'form': form})
 
 
-
-
-
-
-
-
-
-
+@login_required
+def delete_subscription(request, pk):
+    data = get_object_or_404(Companies, pk=pk)
+    data.delete()
+    return redirect('/list_subscriptions/')
